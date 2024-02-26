076425ef6e2202ed93dc75464ae6b3667eba3915
isarmendes <isadoramendes@aluno.senai.br>
2024-02-26 11:27:08 -0300

    navagação completa

--- App.jsx
+++ App.jsx
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Contact from './src/screens/Contact';
+import Home from './src/screens/Home';
+import Profile from './src/screens/Profile';
 
 export default function App() {
+  
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = "Home" component={Home}/>
+        <Stack.Screen name = "Profile" component={Profile}/>
+        <Stack.Screen name = "Contact" component={Contact}/>
+      </Stack.Navigator>
+      <StatusBar style="auto"/>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
